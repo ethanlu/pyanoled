228dce29b6841bf37852136ffcb8e142178a53df
Ethan Lu <fang.lu@gmail.com>
2022-10-03 17:30:51 -0400

    removed ruby-based system script that connects ports and moved it into pyanoled

--- pyanoled/__main__.py
+++ pyanoled/__main__.py
@@ -5,17 +5,35 @@ from pyanoled.event.EventQueue import EventQueue
 from pyanoled.State import State
 from pyanoled.ui.ControlApp import ControlApp
 from pyanoled.visualizer.LEDEngine import LEDEngine
-
+from itertools import combinations
 from logging import config, getLogger, Logger
-from subprocess import call
 
 import concurrent.futures
+import re
+import subprocess
 
 
 class PyanoLED(object):
     def __init__(self, l: Logger, c: Configuration):
         self._l = l
         self._c = c
+        self._port_regex = re.compile(r"^client (\d+): '(.*)'", re.IGNORECASE | re.ASCII)
+
+    def _unlink_ports(self) -> None:
+        subprocess.call('aconnect -x', shell=True)
+
+    def _link_ports(self) -> None:
+        response = subprocess.run('aconnect -l', shell=True, capture_output=True)
+
+        ports = []
+        for l in response.stdout.decode('utf-8').split('\n'):
+            found = self._port_regex.findall(l)
+            if found and int(found[0][0]) > 0:
+                ports.append((found[0][0], found[0][1]))
+
+        for (p1, p2) in combinations(ports):
+            subprocess.call(f"aconnect {p1[0]}:0 {p2[0]:0}", shell=True)
+            subprocess.call(f"aconnect {p2[0]}:0 {p1[0]:0}", shell=True)
 
     def run(self):
         self._l.info('================================== PYANOLED START ==================================')
@@ -23,9 +41,16 @@ class PyanoLED(object):
         event_queue = EventQueue()
         state = State()
 
+        self._unlink_ports()
+        self._link_ports()
+        self._l.info('ports linked...')
+
         while state.is_on() or state.is_reload():
             if state.is_reload():
                 self._l.info('reloading pyanoled...')
+                self._unlink_ports()
+                self._link_ports()
+                self._l.info('ports relinked...')
                 state.on()
 
             ui_thread = ControlApp(getLogger('ui'), self._c, state)
@@ -39,7 +64,7 @@ class PyanoLED(object):
         self._l.info('================================== PYANOLED END ==================================')
 
         if state.is_off():
-            call("sudo shutdown -h now", shell=True)
+            subprocess.call("sudo shutdown -h now", shell=True)
 
 def main():
     c = Configuration(get_conf_path('app.conf'))
