ad97ce680ecbc4ef24045acefcd1aa8cefdb4fbe
Ethan Lu <fang.lu@gmail.com>
2022-10-04 02:14:19 -0400

    updated docker

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name="pyanoled",
-    version="1.1.0",
+    version="1.2.0",
     author="Ethan Lu",
     author_email="fang.lu@gmail.com",
     description="Python Piano LED Visualizer",
